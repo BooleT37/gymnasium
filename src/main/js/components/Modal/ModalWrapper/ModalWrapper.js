bbb8e131faf1019bbe86a87164ae5c6b0744438b
Алексей Левин <BooleT37@gmail.com>
2017-02-14 19:24:45 +0500

    Fix for modal scrolling

--- src/main/js/components/Modal/ModalWrapper/ModalWrapper.js
+++ src/main/js/components/Modal/ModalWrapper/ModalWrapper.js
@@ -9,7 +9,9 @@ export default class ModalWrapper extends React.Component {
     render() {
         return (
             <div className={classnames("modalWrapper", {modalWrapper_blurred: this.props.innerModalOpened === true})}>
-                {this.props.children}
+                <div className="modalWrapper_modalWindow">
+                    {this.props.children}
+                </div>
             </div>
         );
     }
